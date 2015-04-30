622ce83719bc64f2c854e650170643cd4485042f
Angela Ma <angelama@college.harvard.edu>
2015-04-30 18:38:00 -0400

    asdf

--- menu.py
+++ menu.py
@@ -2,24 +2,29 @@ import os, sys
 
 systemName = sys.platform
 
-problemsDirAddress = os.getcwd() + "/Problems"
+directoryAddress = os.getcwd()
 
-problemsDirList = os.listdir(problemsDirAddress)
-problemsDirList.remove("preferences.csv")
+def directorylist_python(dirAddress):
+	dirlist = []
+	for file in os.listdir(dirAddress):
+	  if file.endswith(".py"):
+	  	dirlist.append(file)
+	return dirlist
+
+directoryList = directorylist_python(directoryAddress)
+directoryList.remove("menu.py")
+directoryList.remove("graph_matching.py")
+problemNames = ["Hospital Resident or Stable Marriage", "Stable Roommates"]
+
+print("\n\nMENU for CS51 Final Project\n")
 
 counter = 0
-while counter < len(problemsDirList):
-	print("("+str(count+1)+") " + problemsDirList[count])
-	count += 1
+while counter < len(problemNames):
+	print("("+str(counter+1)+") " + problemNames[counter])
+	counter += 1
 
-print("Welcome to our CS51 Final Project!")
-print("By John Gee, Sam Cheng, Angela Ma, and Emily Wang")
-print("We implemented the solutions to the following problems.")
-print("For the hospital-resident/stable marriage problems, input data into "perferences.csv" in the format specified in the preferences_format file.")
-print("For the stable roommates problems, input data into the csv files in the format specified in the writeup.")
-
-problemNum = int(input("Which problem would you like to solve? (Type a number.)"))
-problemFile = problemsDirList[problemNum - 1]
+problemNum = int(input("\nWhich problem would you like to solve? (Type a number.) "))
+problemFile = directoryList[problemNum - 1]
 
 if systemName == "darwin":
 	os.system("python " + problemFile)
