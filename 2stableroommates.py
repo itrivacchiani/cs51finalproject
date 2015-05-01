65995fb07e3de272c5973282757dae4b1bd81b5a
Angela Ma <angelama@college.harvard.edu>
2015-04-30 23:39:49 -0400

    asdf

--- 2stableroommates.py
+++ 2stableroommates.py
@@ -1,6 +1,6 @@
-import copy, sys, csv
+import copy, sys, csv, os
 
-csv_f = csv.reader(open('preferences.csv'))
+csv_f = csv.reader(open('preferences.csv', 'rU'))
 
 problem = csv_f.next()[0].lower()
 # csv file does not correctly indicate stable marriage or 
@@ -19,9 +19,14 @@ num_roommates = int(csv_f.next()[0])
 
 people = {}
 for i in xrange(0, num_roommates):
-    s1 = csv_f.next()[0]
-    s2 = csv_f.next()[0]
-    people[s1] = s2.split(' ')
+  s1 = csv_f.next()[0]
+  s2 = csv_f.next()[0]
+  people[s1] = s2.split(' ')
+
+for k in people.keys():
+  print("%s "%k)
+  for x in people[k]:
+    print("%s"%x)
 
 def fillRank(people):
   rank = {}
@@ -37,31 +42,32 @@ rank = fillRank(people)
 # this function will return a tuple (y,r) where
 # y is the person of rank i on x's preference list
 # and r is the rank of x in y's preference list.
-def getPersonRank(x,i):
+def GetPersonRank(x,i):
   y = people[x][i]
   r = rank[y][x]
   return (y,r)
 
+leftperson = {} # person 'x' is currently proposing to
+leftrank = {} # rank of that person
+rightperson = {} # person 'x' holds a proposal for
+rightrank = {} # rank of that person
+holds_proposal = {} # boolean
+
 # PhaseI creates plausible semi-matchings 
 # between roommates
 def phaseI(): 
-  leftperson = {} # person 'x' is currently proposing to
-  leftrank = {} # rank of that person
-  rightperson = {} # person 'x' holds a proposal for
-  rightrank = {} # rank of that person
-  holds_proposal = {} # boolean
 
   # initialization
   for k in people.keys():
     n = len(people.keys())
-    holds_proposal[k] = false
+    holds_proposal[k] = False
     rightperson[k] = k 
     rightrank[k] = n
     leftrank[k] = 1
   # creates semi-engagements (plausible matches)
   for k in people.keys():
     proposer = k
-    while true: 
+    while True: 
       (next, rank) = GetPersonRank(proposer, leftrank[proposer])
       while (rank > rightrank[next]):
         leftrank[proposer] = leftrank[proposer] + 1 
@@ -71,31 +77,32 @@ def phaseI():
       rightperson[next] = proposer
       leftperson[proposer] = next
       proposer = previous 
-      if (holds_proposal[next] == false): 
+      if (holds_proposal[next] == False): 
         break 
-    holds_proposal[next] = true
+    holds_proposal[next] = True
     if (leftrank[proposer] == n):
-      return false # can only be engaged to self, no stable match
-  return true # move onto phaseII, there may be stable match
+      return False # can only be engaged to self, no stable match
+  return True # move onto phaseII, there may be stable match
+
+cycle = []
 
 def seekCycle(): 
-  cycle = []
 
   for k in people.keys():
     if (leftrank[k] < rightrank[k]):
       break
-  if (leftrank[k] >== rightrank[k]):
+  if (leftrank[k] >= rightrank[k]):
     return (0,0,"")
-  else 
+  else :
     last = 1 
-    while true:
+    while True:
       cycle[last] = k
       last = last + 1 
       p = leftrank[k]
-      while true: 
+      while True: 
         p = p + 1 
         (y,r) = GetPersonRank(k,p)
-        if (r <== rightrank[y]):
+        if (r <= rightrank[y]):
           break
       secondrank[k] = p
       secondperson[k] = y
@@ -105,18 +112,20 @@ def seekCycle():
         break 
     last = last - 1
     first = last - 1
-    while (cycle[first] !== k):
+    while (cycle[first] != k):
       first = first - 1 
     return (first, last, cycle)
 
+cycle2 = []
+
 def phaseII():
-  solution_possible = true
-  solution_found = false 
-  while (solution_possible && !solution_found):
+  solution_possible = True
+  solution_found = False 
+  while (solution_possible and not solution_found):
     (first, last, cycle) = seekCycle()
     if (cycle == ""):
-      solution_found = true 
-    else 
+      solution_found = True 
+    else:
       cycle2 = cycle[first:last]
       for i in range(cycle2):
         leftrank[k] = secondrank[k]
@@ -124,8 +133,24 @@ def phaseII():
         rightperson[leftperson[k]] = k
       for i in range(cycle2):
         if (leftrank[k] > rightrank[k]):
-          sol_possible = false 
+          sol_possible = False 
   return solution_found
 
+print("hi")
+
+if not phaseI:
+  print("There is no possible stable roommates arrangement")
+  os.system("python menu.py")
+else:
+  if not phaseII:
+    print("There is no solution.")
+    os.system("python menu.py")
+  else:
+    print ("hi")
+    for i in cycle2:
+      print("%s" % i)
+    for i in cycle:
+      print("%s" % i)
+      print("hi")
 
 
